bd10e7b1d9df44cd4e9d9ad8eda3d8abe095fcfb
subtlemocha00 <kturley@gmail.com>
2024-07-31 13:54:54 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+	const [heading, setHeading] = useState("Magnificent Monkeys");
+	const clickHandler = () => {
+		setHeading("Radical Rhinos");
+	};
+	return (
+		<>
+			<button type="button" onClick={clickHandler}>
+				Click Me!
+			</button>
+			<h1>{heading}</h1>
+		</>
+	);
+};
+
+export default App;
